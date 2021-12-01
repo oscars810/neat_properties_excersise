555a13eacac8c5380c2c05129a8dc07ff3bd8f5d
Oscar <oscar.carcamoz@uc.cl>
2021-12-01 12:52:15 -0300

    fix(main): reorder lines

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { simulateData } from "./utils";
 import Analitycs from "./analitycs";
 
-const analitycsSystem = new Analitycs();
-var date1 = new Date("March 13, 2020");
-var date2 = new Date("November 28, 2020");
 var data = simulateData();
 const portfolio1 = data[0];
 
+const analitycsSystem = new Analitycs();
+
+var date1 = new Date("March 13, 2020");
+var date2 = new Date("November 28, 2020");
 analitycsSystem.Profit(portfolio1, date1, date2);
+
 var date1 = new Date("March 13, 2020");
 var date2 = new Date("November 28, 2020");
 analitycsSystem.IdealProfit(portfolio1, date1, date2);
